adc01446f2653bd88a8362dc68d2c4eb3938063a
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-12-30 16:18:18 +0700

    fix(ui): adjust header layout and improve spacing for better responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+     screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      
+      "xl-max": { max: "1440px" },
+      "lg-max": { max: "1024px" },
+      "md-max": { max: "768px" },
+      "sm-max": { max: "576px" },
+    },
+
     extend: {
       colors: {
         'xd-yellow': '#F2B900',
